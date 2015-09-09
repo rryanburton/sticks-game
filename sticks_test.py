36fd98866c738e1cee26a8d5485c8e74d29435f1
Ryan Burton <rryanburton@gmail.com>
2015-09-09 00:38:39 -0400

    streamlined test file for future tests

--- sticks_test.py
+++ sticks_test.py
@@ -1,1 +1,18 @@
-sticks_test.py
+# sticks_test.py
+import unittest
+import random
+from sticks import *
+
+
+class TestSticks(unittest.TestCase):
+
+
+    def test_game_loop(self):
+        # self.assertEqual(easy_words(word_list),
+        # easy_words_list = []
+
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
